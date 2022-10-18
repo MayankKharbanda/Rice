1fa465af9e8d7925f29d8c6b29ca33464d96046c
Mayank Kharbanda <mayankk@iiitd.ac.in>
2022-10-18 19:24:42 +0530

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+
+fish_logo cyan green blue
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
