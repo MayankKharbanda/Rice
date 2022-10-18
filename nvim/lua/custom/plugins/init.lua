aa42610e6beaae12b0f9d21887320e7424721b48
Mayank Kharbanda <mayankk@iiitd.ac.in>
2022-10-18 19:25:51 +0530

    Add files via upload

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+
+   ["elkowar/yuck.vim"] = { ft = "yuck" },
+
+    ["NvChad/nvterm"] = {
+      config = function()
+         require "plugins.configs.nvterm"
+      end,
+   },
+
+   ['lervag/vimtex'] = {}
+}
